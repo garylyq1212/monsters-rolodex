c58fb27e53b2ccac1c23bbb9a38e470aa7cd63ce
GaryQuan <garylyq17@gmail.com>
2019-07-03 13:28:42 +0800

    Updates

---
+++ precache-manifest.9738b99e26770c8c50e7a6a5fea2c220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66689abf23267fa0faad28819740c01a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc9164e6cec343140118",
+    "url": "/monsters-rolodex/static/css/main.c53c8c5f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fc9164e6cec343140118",
+    "url": "/monsters-rolodex/static/js/main.f0da85db.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
